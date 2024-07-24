99a88acf11e01aa7d1f147ec13f8db78fde17b57
reza shabani <rezashabani.cs@gmail.com>
2024-07-25 00:09:13 +0330

    Create my_plugin.h

---
+++ vpp_plugin/my_plugin.h
@@ -0,0 +1,9 @@
+#ifndef __included_my_plugin_h__
+#define __included_my_plugin_h__
+
+typedef enum {
+  MY_PLUGIN_NEXT_INTERFACE_OUTPUT,
+  MY_PLUGIN_N_NEXT,
+} my_plugin_next_t;
+
+#endif /* __included_my_plugin_h__ */
